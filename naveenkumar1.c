3c72fe6eb7570970facff7090a7fd558d6be805b
naveenmariahavel <35653387+naveenmariahavel@users.noreply.github.com>
2018-01-30 15:55:33 +0530

    Create naveenkumar1.c

---
+++ naveenkumar1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int num;
+printf("enter the number:",num);
+scanf("%d",&num);
+if(num>0)
+printf("%d is a positive number",num);
+else if (num<0)
+printf("%d is a negative number",num);
+else
+printf("%d is zero:,num); 
+return 0;
+}
