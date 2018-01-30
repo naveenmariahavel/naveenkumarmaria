26df992e34870fd2d6ddaa97dcd8d970fd34a894
naveenmariahavel <35653387+naveenmariahavel@users.noreply.github.com>
2018-01-30 16:31:33 +0530

    Create naveenkumar9.c

---
+++ naveenkumar9.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,k,a[100],i,s=0;
+clrscr();
+scanf(%d%d",&n,&k);
+for(i=0,i<n,i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0,i<k,i++)
+{
+s=s+a[i];
+}
+printf("%d",s)
+}
+getch();
+}
