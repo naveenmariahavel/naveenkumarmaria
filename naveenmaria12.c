88fe3cfcdc0c4526fc97502558284b91f9f5499c
naveenmariahavel <35653387+naveenmariahavel@users.noreply.github.com>
2018-02-09 16:09:44 +0530

    Create naveenmaria12.c

---
+++ naveenmaria12.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int i,n,c=1,p;
+    clrscr();
+    
+    scanf("%d",&n);
+    scanf("%d",&p);
+    for(i=0;i<p;i++)
+    {
+        c=c*n;
+        
+    }
+     printf("%d",c);
+     getch();
+}
