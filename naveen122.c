ae6f000bc2c28905e99d768ca35319d6869dff8c
naveenmariahavel <35653387+naveenmariahavel@users.noreply.github.com>
2018-02-09 16:23:28 +0530

    Create naveen122.c

---
+++ naveen122.c
@@ -0,0 +1,31 @@
+
+#include<stdio.h>
+void main()
+print("Enter a number to check whether it is palindrome or not”);
+a=int(input())
+
+print(a)
+
+n=0
+
+temp=a
+
+rev=0
+
+while(temp>0):
+
+rem=temp%10
+
+rev=(rev*10)+rem
+
+temp=int(temp/10)
+
+if(rev==a):
+
+print("Yes,it is a palindrome!!!")
+
+else:
+
+print("No,it is not a palindrome!")
+}
+
