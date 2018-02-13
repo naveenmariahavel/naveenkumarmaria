a1d218a15a2d97fdf93a617d7f08228ef0bb36a6
naveenmariahavel <35653387+naveenmariahavel@users.noreply.github.com>
2018-02-13 15:51:38 +0530

    Create naveenmaria18.c

---
+++ naveenmaria18.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num,rem=0,cub=0;sum=0,temp;
+printf("enter the num:");
+scanf("%d",&n);
+while(num!=0)
+{
+rem=num%10;
+cub=pow(rem,3);
+sum=sum+cube;num=num/10;
+if(num==temp)
+
+printf("The num is amstrong",num);
+else
+printf("The num is not amstrong",num);
+}
+}
