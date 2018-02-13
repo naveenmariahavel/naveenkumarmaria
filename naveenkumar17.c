b74fbc9ac8d7fdccf4ef4e21f5e117f75569d824
naveenmariahavel <35653387+naveenmariahavel@users.noreply.github.com>
2018-02-13 16:02:39 +0530

    Create naveenkumar17.c

---
+++ naveenkumar17.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,rem;
+for(i=0;i<n;i++)
+{
+printf("enter the number");
+scanf("%d",&n);
+  remainder=total+i;
+  total=remainder;
+if(n==armstrong)
+{
+printf("the number is armstrong");
+}
+else
+{
+printf("the number is not armstrong");
+}
+}
+}
