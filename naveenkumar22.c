9467024d7536d2d575e5a9161b0709791eb08e93
naveenmariahavel <35653387+naveenmariahavel@users.noreply.github.com>
2018-02-13 16:37:49 +0530

    Create naveenkumar22.c

---
+++ naveenkumar22.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main(void) 
+{  
+int n,i,a[n],max;  
+scanf("%d",&n);
+for(i=0;i<n;i++) 
+scanf("%d",&a[i]);
+
+	max=a[0];
+	for(i=1;i<n;i++)
+	{ 
+	if(a[i]>max)
+	{
+		max=a[i];
+	}
+	}
+	printf("%d",max);
+
+
+return 0;
+}
