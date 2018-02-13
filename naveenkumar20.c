3afb4b6e006b90fce1cfeaeb870455bc2c395a0b
naveenmariahavel <35653387+naveenmariahavel@users.noreply.github.com>
2018-02-13 16:21:25 +0530

    Create naveenkumar20.c

---
+++ naveenkumar20.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i;
+int n,a;
+printf("enter then number");
+scanf("%d",n);
+for(i=0;i<=n;i++)
+{
+a=i*5;
+printf("the muliple numbers are %d",a);
+}
+getch();
+}
