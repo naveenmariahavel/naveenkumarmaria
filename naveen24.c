db3132985b17fd20582ecff2b4ab802a362bb98d
naveenmariahavel <35653387+naveenmariahavel@users.noreply.github.com>
2018-02-20 15:56:43 +0530

    Create naveen24.c

---
+++ naveen24.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+{
+int a[100],n,i,j,temp;
+printf("enter the limit");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(a[i]>a[j]))
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+}
+for(i=0;i<n;i++)
+{
+printf("%d\t",a[i]);
+}
+return 0;
+}
