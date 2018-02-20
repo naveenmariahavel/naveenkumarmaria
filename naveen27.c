2960ba5217dbd937f6ddc546719f19d69619a4ef
naveenmariahavel <35653387+naveenmariahavel@users.noreply.github.com>
2018-02-20 16:29:08 +0530

    Create naveen27.c

---
+++ naveen27.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char s[10]; int l,i,d=0;
+gets(s);
+l=strlen(s);
+for(i=0;i<l;i++)
+{
+if(s[i]>='0'&&s[i]<='9')
+{
+d++;
+}
+else
+{
+printf("\nnot numeric");
+break;
+}
+}
+if(l==d)
+{
+printf("\n numeric");
+}
+return 0;
+}
