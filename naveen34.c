c2ffbde244bf00b5ea500bd492c38c8ba4c2e2e6
naveenmariahavel <35653387+naveenmariahavel@users.noreply.github.com>
2018-03-02 13:57:52 +0530

    Create naveen34.c

---
+++ naveen34.c
@@ -0,0 +1,19 @@
+#incude<stdio.h>
+#include<conio.h>
+void main()
+{
+char str[30];
+int i=0,count=0;
+  printf("enter the string");
+  scanf("%s",&str);
+while(str[i]!='\0')
+{
+if(str[i]=="\n")
+{
+++count;
+}
+++i;
+}
+count++;
+printf("no of lines is %d ",count);
+}
