6b88e3a9dc9f77b4326e2aa31bcb399d45703028
naveenmariahavel <35653387+naveenmariahavel@users.noreply.github.com>
2018-03-13 15:57:58 +0530

    Create naveen44.c

---
+++ naveen44.c
@@ -0,0 +1,20 @@
+#include<>stdio.h>
+int main()
+{
+int i,g;
+printf("enter the value of i");
+scanf("%d",&i);
+for(i=1;i<=10;i++)
+{
+printf("%d",i);
+}
+if(g==i)
+{
+pritf("yes");
+}
+else
+{
+printf("no");
+}
+return 0;
+}
