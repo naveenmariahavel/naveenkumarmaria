c2064d07ce9dc4b183ff5de76d147a18e38e73ae
naveenmariahavel <35653387+naveenmariahavel@users.noreply.github.com>
2018-03-16 14:23:15 +0530

    Create naveen64.c

---
+++ naveen64.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int a,b;
+printf("enter a first number");
+scanf("%d",&a);
+printf("enter a second number"):
+scanf("%d",&b);
+if(9%2==0)
+{
+printf("odd");
+}
+else
+{
+printf("even");
+}
