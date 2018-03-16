02c6dd2c42ad97075f957a25ae98e72134870b44
naveenmariahavel <35653387+naveenmariahavel@users.noreply.github.com>
2018-03-16 14:48:54 +0530

    Create naveen681.c

---
+++ naveen681.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+   int num;
+    scanf("%d",&num);
+    if(num%7==0)
+    {
+        printf("\n multiple of 7");
+    }
+    else
+    {
+    printf("\n Not a multiple of 7");
+    }
+    }
